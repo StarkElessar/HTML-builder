d163df8e31491bf30b14cddc4017df12a718a682
Stark <serj.elessar@gmail.com>
2022-11-06 21:40:22 +0300

    fix: output file txt to console

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,3 +2,6 @@ const path = require('path')
 const fs = require('fs')
 // Абсолютный путь к моему читаемому файлу:
 const pathToTXTFile = path.join(__dirname, 'text.txt')
+
+fs.createReadStream(pathToTXTFile, { encoding: 'utf-8' })
+  .on('data', (text) => console.log(text))
