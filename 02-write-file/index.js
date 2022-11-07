e5b246652a6cec61806265fa3c37e54323d891ec
Stark <serj.elessar@gmail.com>
2022-11-07 21:25:13 +0300

    feat: output to console stdin/stdout

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,4 +1,6 @@
 const { stdin, stdout } = process
+const fs = require('fs')
 
+const outputFileTXT = fs.createWriteStream('log.txt')
 
-console.log(stdin);
+stdout.write('Здарова! \n Внизу после этого сообщения ты можешь ввести своё сообщение и я запишу его в файл логирования "log.txt". \n Ты можешь закончить этот процесс, просто введи в консоли "exit" или нажми "CTRL+C \n')
