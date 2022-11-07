d3f1592b7a1052309da2bfb099425fc9fbf8162d
Stark <serj.elessar@gmail.com>
2022-11-07 21:48:52 +0300

    fix: final version

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,14 +1,23 @@
-const { stdin, stdout } = process
 const fs = require('fs')
+const path = require('path')
+const { stdin, stdout } = process
 
-const outputFileTXT = fs.createWriteStream('log.txt')
+const outputFileTXT = fs.createWriteStream(path.join(__dirname, 'log.txt'))
 
-stdout.write(' Здарова! \n Внизу после этого сообщения ты можешь ввести своё сообщение и я запишу его в файл логирования "log.txt". \n Ты можешь закончить этот процесс, просто введи в консоли "exit" или нажми "CTRL+C" \n')
+stdout.write(
+  ' Здарова! \n Внизу после этого сообщения ты можешь ввести своё сообщение и я запишу его в файл логирования "log.txt". \n Ты можешь закончить этот процесс, просто введи в консоли "exit" или нажми "CTRL+C" \n'
+)
 
 stdin.on('data', (chunk) => {
   if (chunk.toString().trim() === 'exit') {
-    stdout.write(' Ввод завершен..\n До следующего сеанса!\n')
-    process.exit();
+    stdout.write(' Файл записан..\n До следующего сеанса!\n')
+    process.exit()
   }
-  output.write(chunk);
+
+  outputFileTXT.write(chunk)
+})
+
+process.on('SIGINT', () => {
+  stdout.write(' Файл записан..\n До следующего сеанса!\n')
+  process.exit()
 })
