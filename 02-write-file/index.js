5cd961c6f21b2e8790badc2563bdade68cd355cd
Stark <serj.elessar@gmail.com>
2022-11-07 21:28:12 +0300

    fix: add the ability to exit the console

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,4 +3,12 @@ const fs = require('fs')
 
 const outputFileTXT = fs.createWriteStream('log.txt')
 
-stdout.write('Здарова! \n Внизу после этого сообщения ты можешь ввести своё сообщение и я запишу его в файл логирования "log.txt". \n Ты можешь закончить этот процесс, просто введи в консоли "exit" или нажми "CTRL+C \n')
+stdout.write(' Здарова! \n Внизу после этого сообщения ты можешь ввести своё сообщение и я запишу его в файл логирования "log.txt". \n Ты можешь закончить этот процесс, просто введи в консоли "exit" или нажми "CTRL+C" \n')
+
+stdin.on('data', (chunk) => {
+  if (chunk.toString().trim() === 'exit') {
+    stdout.write(' Ввод завершен..\n До следующего сеанса!')
+    process.exit();
+  }
+  output.write(chunk);
+})
