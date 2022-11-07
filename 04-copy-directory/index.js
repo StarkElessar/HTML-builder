ee1e3322e223a72b36fe885dc16af8b024dbcf9c
Stark <serj.elessar@gmail.com>
2022-11-07 22:58:17 +0300

    fix: delete sync func

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const fs = require('fs')
 const { stdout } = require('process')
 const { mkdir, readdir, copyFile, rm } = require('fs/promises')
 
@@ -10,8 +9,6 @@ const copyFolder = async () => {
   try {
     // Удаляем папку если она есть
     await rm(pathCopyDir, { force: true, recursive: true })
-    if (fs.existsSync(pathCopyDir)) {
-    }
     // Создаем новую папку
     await mkdir(pathCopyDir, { recursive: true })
 
